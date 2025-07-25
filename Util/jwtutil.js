9d951fdf2509745209af4fbb0bcccd113f6882f4
Susovan700 <susovansinhababu@gmail.com>
2025-07-25 11:31:56 +0530

    kkkkl

--- Util/jwtutil.js
+++ Util/jwtutil.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
 
-// const SECRET_KEY = process.env.JWT_SECRET;
+const SECRET_KEY = process.env.JWT_SECRET;
 
 export function generatejwt(user_id, email) {
-  return jwt.sign({ user_id, email }, "He,kewnjkjnfljehyfjwehbkjeshys,ebfjhsebhjf", {
+  return jwt.sign({ user_id, email }, "SECRET_KEY", {
     expiresIn: "3h",
   });
 }
 
 export function verifyjwt(token) {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, "He,kewnjkjnfljehyfjwehbkjeshys,ebfjhsebhjf", (err, decoded) => {
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
       if (err) {
         reject("Invalid Token");
       } else {
